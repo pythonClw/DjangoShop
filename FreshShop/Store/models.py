ae76d693b2b009185295082435002b3a79de8bbe
clwLoving@qq.com <clwLoving@qq.com>
2019-07-22 15:20:46 +0800

    数据库同步和静态文件配置

--- FreshShop/Store/models.py
+++ FreshShop/Store/models.py
@@ -8,4 +8,25 @@ class Seller(models.Model):
     picture = models.ImageField(upload_to="store/images", verbose_name="头像",null=True,blank=True)
     address = models.CharField(max_length=32,verbose_name="地址",null=True,blank=True)
     card_id = models.CharField(max_length=32,verbose_name="身份证号",null=True,blank=True)
-
+class StoreType(models.Model):
+    store_type = models.CharField(max_length=32,verbose_name="类型名称")
+    type_description = models.TextField(verbose_name="类型描述")
+class Store(models.Model):
+    store_name = models.CharField(max_length=32,verbose_name="店铺名称")
+    store_address = models.CharField(max_length=32,verbose_name="店铺地址")
+    store_description = models.TextField(verbose_name="店铺描述")
+    store_logo = models.ImageField(upload_to="store/images",verbose_name="店铺logo")
+    store_phone = models.CharField(max_length=32,verbose_name="店铺注册资金")
+    user_id = models.IntegerField(verbose_name="店铺主人")
+    type = models.ManyToManyField(to=StoreType,verbose_name="店铺类型")
+class Goods(models.Model):
+    goods_name = models.CharField(max_length=32,verbose_name="商品名称")
+    good_price = models.FloatField(verbose_name="商品价格")
+    good_image = models.ImageField(upload_to="store/images",verbose_name="商品图片")
+    goods_number = models.IntegerField(verbose_name="商品库存")
+    goods_description = models.DateField(verbose_name="出厂日期")
+    goods_safeDate = models.IntegerField(verbose_name="保质期")
+class GoodImg(models.Model):
+    img_address = models.ImageField(upload_to="store/images",verbose_name="图片地址")
+    img_description = models.TextField(max_length=32,verbose_name="图片描述")
+    goods_id = models.ForeignKey(to=Goods,on_delete=models.CASCADE,verbose_name="商品id")
